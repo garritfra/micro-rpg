4b23fd80578e2e693dcd41060ad288c95a0bd70f
garritfra <garritfranke@gmail.com>
2019-01-27 19:33:19 +0100

    Add networking

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import request from "request";
 
 class App extends React.Component {
   render() {
+    request("http://inventory:8080/", (err, res, body) => {
+      console.log(body);
+    });
     return <h1>Hello Frontend</h1>;
   }
 }
